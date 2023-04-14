dbf49b7c4a841271e501e2c0c1477ad7ca95d39b
Thalles Gössling <thalles_3@hotmail.com>
2023-04-13 22:34:04 -0300

    Add the solution of the rock, paper and scissors challenge solved.

---
+++ javascript.js
@@ -0,0 +1,51 @@
+function getComputerChoice() {
+    const choices = ["Rock", "Paper", "Scissors"];    
+    return choices[Math.floor(Math.random() * choices.length)].toLowerCase();    
+  }
+
+function playRound() {
+    let scorePlayer = 0;
+    let scorePC = 0;
+    
+    while ((scorePC + scorePlayer) < 5) {
+        let player = prompt("Digite a sua escolha: ").toLowerCase();
+        let pc = getComputerChoice();
+
+        if (player == pc) {
+            alert("It is a draw!");
+        }
+        else {
+            if (player == "rock" && pc == "paper") {
+                alert("You lost!");
+                scorePC++;
+            }
+            else if (player == "rock" && pc == "scissors") {
+                alert("You won!");
+                scorePlayer++;
+            }
+            else if (player == "paper" && pc == "scissors") {
+                alert("You lost!");
+                scorePC++;
+            }
+            else if (player == "paper" && pc == "rock") {
+                alert("You won!");
+                scorePlayer++;
+            }
+            else if (player == "scissors" && pc == "rock") {
+                alert("You lost!");
+                scorePC++;
+            }
+            else if (player == "scissors" && pc == "paper") {
+                alert("You won!");
+                scorePlayer++;
+            }
+        }
+    }
+
+    return "The players had these scores: User " + scorePlayer + " points and Computer " + scorePC + " points.";
+}  
+
+console.log(playRound());
+
+
+  
